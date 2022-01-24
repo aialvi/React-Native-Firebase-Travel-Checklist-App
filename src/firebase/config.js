25f45117ea28b00f870f0854e85f66d3bdd7ebfe
Aminul Islam Alvi <aminulialvi@gmail.com>
2022-01-24 23:43:46 +0600

    Add firebase configuration

---
+++ src/firebase/config.js
@@ -0,0 +1,28 @@
+import * as firebase from "firebase";
+import "@firebase/auth";
+import "@firebase/firestore";
+import {
+  apiKey,
+  authDomain,
+  projectId,
+  storageBucket,
+  messagingSenderId,
+  appId,
+  measurementId,
+} from "@env";
+
+const firebaseConfig = {
+  apiKey: apiKey,
+  authDomain: authDomain,
+  databaseURL: databaseURL,
+  projectId: projectId,
+  storageBucket: storageBucket,
+  messagingSenderId: messagingSenderId,
+  appId: appId,
+};
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
+
+export { firebase };
