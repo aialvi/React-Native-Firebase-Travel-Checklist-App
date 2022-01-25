8d3cc776cdbe0c3242ab00dbbf2571a1812eb23f
Aminul Islam Alvi <aminulialvi@gmail.com>
2022-01-25 11:55:11 +0600

    Login Page

--- App.tsx
+++ App.tsx
@@ -1,6 +1,5 @@
 import { StyleSheet } from "react-native";
 
-import "react-native-gesture-handler";
 import React, { useState } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
